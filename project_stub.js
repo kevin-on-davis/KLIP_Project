885695ca1107a97b1649c6462b5329ae7c119fed
David <david.bland@hotmail.com>
2019-10-18 15:06:09 -0400

    changes done to get mapbox working

--- project_stub.js
+++ project_stub.js
@@ -5,19 +5,20 @@ function getMyLocationURL() {
     // var latitude;
     var longitude = -79;
     var latitude = 43;
-    debugger;
+    // debugger;
     if (navigator.geolocation) 
     {
         navigator.geolocation.getCurrentPosition(success, error);
         
         function success(pos)
         {
-            // longitude = pos.coords.longitude;
-            // latitude = pos.coords.latitude;
+
+            longitude = pos.coords.longitude;
+            latitude = pos.coords.latitude;
             console.log(longitude+"-"+latitude);
             // myMarker = `{pin-l}-{Home}+{blue}(${longitude},${latitude})`;
-            queryPlaceURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${api_key}`;
-            queryMapURL = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${longitude},${latitude},14.25,0,60/600x600?access_token=${api_key}`;
+            // queryPlaceURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${api_key}`;
+            // queryMapURL = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${longitude},${latitude},14.25,0,60/600x600?access_token=${api_key}`;
             // console.log(queryMapURL);
             // console.log(queryPlaceURL);
 
@@ -27,7 +28,7 @@ function getMyLocationURL() {
                 container: 'map', // container id
                 style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
                 center: [longitude, latitude], // starting position [lng, lat]
-                zoom: 1, // starting zoom
+                zoom: 12, // starting zoom
             });
         }
 
