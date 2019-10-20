f7d289292feeb81e1e2adf30fc5f340bdcfebf3b
kevin-on-davis <kevin.on.davis@gmail.com>
2019-10-20 08:46:16 -0400

    Testing button functionality

--- bookshelf.js
+++ bookshelf.js
@@ -1,39 +1,52 @@
 function manageBookShelf()
 {
+    debugger;
     if (!localStorage.myBookShelf)
     {
         var bookshelf = [];
-        var shelfItem = [];
+        var shelfItem = {"title":"The Killing Dance", "author":"Laurel K. Hamilton"};
+        // console.log(shelfItem);
         bookshelf.push(shelfItem);
+        console.log(bookshelf);
     }
     else
     {
         bookshelf = JSON.parse(localStorage.getItem("myBookShelf"));
+        shelfItem = {"title":"Obsidian Butterfly", "author":"Laurel K. Hamilton"};
+        if (!bookshelf.some(e => e.title === shelfItem.title))
+        {
+            bookshelf.push(shelfItem);
+        };
     }
-
-    for (i=0; i < shelfItem.length; i++)
+    localStorage.setItem("myBookShelf", JSON.stringify(bookshelf));
+    $("#bookshelf").attr("size", Math.min(bookshelf.length, 20));
+    for (i=0; i < bookshelf.length; i++)
     {
-        $("#bookshelf").append(`<div>Book Information<button id="buyBook"></button><button id="borrowBook"></button><button id="deleteBook"></button></div>`);
+        console.log(bookshelf);
+        // $("#bookshelf").append(`<option><div><span>${bookshelf[i].title} ${bookshelf[i].author}</span><button id="buyBook"></button><button id="borrowBook"></button></div></option>`);
+        $("#bookshelf").append(`<div class="col-4" id="shelf" style="padding:1vw; border:solid"><span>${bookshelf[i].title} ${bookshelf[i].author} <button id="buyBook">Buy Book</button> <button id="borrowBook">Borrow Book</button> <button id="deleteBook">X</button></span></div>`);
     }
-}
+};
 
 function buildBookshelf()
 {
-    $("#body").append(`<div class="container"><select class="row" id="bookshelf"></select></div>`);
+    $("body").append(`<div class="row col-4" id="bookshelf"></div>`);
+    manageBookShelf();
 };
 
 btn_buybook = $("#buyBook");
 btn_brwbook = $("#borrowBook");
 btn_delbook = $("#deleteBook");
 
-btn_buybook.on("click", "option", function()
+btn_brwbook.on("click", "option", getMyLocationURL);
+
+btn_delbook.on("click", function()
 {
-    console.log("Buy book online");
+    // for (1=0; i < bookshelf.length; i++)
+    // {
+    //     if (this.button)
+    // };
 });
-
-btn_brwbook.on("click", getMyLocationURL());
-
-
 
 function getMyLocationURL() 
 {
@@ -62,7 +75,7 @@ function getMyLocationURL()
                 // zoom: 12, // starting zoom
             });
 
-            var marker = new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
+            var marker_0 = new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
         }
 
         function error(err) 
