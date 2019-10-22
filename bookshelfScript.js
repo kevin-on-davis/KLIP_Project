a69f535b0d196f632fa0c2ecd8f01a1fdb6996a7
Patrick Abromeit <patrick.abromeit@gmail.com>
2019-10-21 22:51:33 -0400

    fixed the map function, map tiles are now aligned

--- bookshelfScript.js
+++ bookshelfScript.js
@@ -46,7 +46,7 @@ $("#picture").empty();
     $("#isbn").append("ISBN: " + response.items[0].volumeInfo.industryIdentifiers[1].identifier);
 
     $("#reviews").empty();
-    $("#reviews").append("Avg. Rating: " + response.items[0].volumeInfo.averageRating);
+    $("#reviews").append("Avg. Rating: " + response.items[0].volumeInfo.averageRating + "/5");
 
     //var isbn = response.items[0].volumeInfo.industryIdentifiers[1];
 }
@@ -111,7 +111,6 @@ function loadBookShelf()
 };
 
 
-
 var btn_options = $("#savedBook");
 var lst_bookseller = $("#booksellers");
 
@@ -163,45 +162,45 @@ btn_options.on("click", ".option", function()
 });
 
 function getMyLocationURL() 
-{
-    var longitude
-    var latitude
-	var mymap = L.map('mapid').setView([43.65,  -79.39], 11);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'
-    }).addTo(mymap);
-
+{   
     if (navigator.geolocation)
-   {
+    {
        navigator.geolocation.getCurrentPosition(success, error);
        function success(pos)
        {
-           longitude = pos.coords.longitude;
-           latitude = pos.coords.latitude;
-           console.log(longitude)
-           console.log(latitude)
+            $("#mapBox").empty()
+            $("<div>").attr({
+                style: "width: 600px; height: 400px; position: absolute",
+                id: "mapid"
+            }).appendTo("#mapBox")
+        
+            var longitude
+            var latitude
+            var mymap = L.map('mapid').setView([43.65,  -79.39], 11);
+            
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGFicm9tZSIsImEiOiJjazF1N3M0aTQwYWYxM21vamhwenRrZ3FwIn0.U1Kj3RdOyh3OX2JIuKBvAw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
+            }).addTo(mymap);
 
-            searchBorder = (longitude-0.05).toFixed(6) + "," + (latitude-0.05).toFixed(6) + "," + (longitude+0.05).toFixed(6) + "," + (latitude+0.05).toFixed(6)
-            console.log(searchBorder)
-            // searchBorder = "-79.397015,43.601070,-79.307015,43.691070"
-            // console.log(searchBorder)
-            searchTerm = "starbucks"
+            longitude = pos.coords.longitude;
+            latitude = pos.coords.latitude;
+
+            searchBorder = (longitude-0.025).toFixed(6) + "," + (latitude-0.025).toFixed(6) + "," + (longitude+0.025).toFixed(6) + "," + (latitude+0.025).toFixed(6)
+            searchTerm = "library"
             // toronto = "-79.347015,43.651070" 
             
-            url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchTerm}.json?bbox=${searchBorder}&access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw`
+            url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchTerm}.json?bbox=${searchBorder}&limit=10&access_token=pk.eyJ1IjoicGFicm9tZSIsImEiOiJjazF1N3M0aTQwYWYxM21vamhwenRrZ3FwIn0.U1Kj3RdOyh3OX2JIuKBvAw`
 
+            console.log(url)
             $.get(url).then(function(response){
                 console.log(response)
                 for (x = 0; x <= response.features.length; x++){
                     longitude = response.features[x].center[0]
                     latitude = response.features[x].center[1]
-                    // console.log(longitude)
-                    // console.log(latitude)
                     var marker = L.marker([latitude,longitude ]).addTo(mymap);
                     marker.bindPopup("<b>" + response.features[x].text + "</b><br>" + response.features[x].properties.address);
                 }
